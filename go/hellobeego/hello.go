60d9dc57c319ba038c26655c8ca19917c8bccb82
Yili He <eli.he999@hotmail.com>
2021-05-20 11:22:38 +0800

    <feat> Open browser

--- go/hellobeego/hello.go
+++ go/hellobeego/hello.go
@@ -9,18 +9,37 @@
 package main
 
 import (
-    "github.com/beego/beego/v2/server/web"
+	"os/exec"
+	"runtime"
+
+	"github.com/beego/beego/v2/server/web"
 )
 
+func openBrowser(url string) error {
+	var cmd string
+	var args []string
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	return exec.Command(cmd, append(args, url)...).Start()
+}
+
 type MainController struct {
-    web.Controller
+	web.Controller
 }
 
 func (this *MainController) Get() {
-    this.Ctx.WriteString("hello world")
+	this.Ctx.WriteString("hello world")
 }
 
 func main() {
-    web.Router("/", &MainController{})
-    web.Run()
+	openBrowser("http://localhost:8080")
+	web.Router("/", &MainController{})
+	web.Run()
 }
