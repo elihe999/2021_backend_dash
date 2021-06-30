52856d0ca2a99be657a343701cd6e9cdf9763e97
Yili He <eli.he999@hotmail.com>
2021-06-30 17:14:33 +0800

    <chore>Update day5

---
+++ php/laravel_test/day5/laravel57/resources/views/request/login.blade.php
@@ -0,0 +1,6 @@
+<form method="post" action="requestNocsrf/1111">
+      {{--  {{csrf_field()}}--}}
+    用户：<input type="text" name="username"><br>
+    密码：<input type="text" name="password"><br>
+    <button type="submit">提交</button>
+</form>
