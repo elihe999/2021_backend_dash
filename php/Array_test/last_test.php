6cbb5a787056def41bd446e917352b41664bef09
blockgolde <eli.he999@hotmail.com>
2022-07-12 14:16:27 +0800

    Chore: Add test example

---
+++ php/Array_test/last_test.php
@@ -0,0 +1,5 @@
+<?php
+
+$array = ['adfadf'=> 1, 'dfdf'=>23];
+var_dump(end($array));
+?>
---
+++ php/wfwrite/str_slashes.php
@@ -0,0 +1,6 @@
+<?php
+
+// addslashes
+$str = addslashes('Shanghai \n is the "biggest" city in China.');
+echo($str);
+?>
